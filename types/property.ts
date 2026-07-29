143b638bb1731fe4086e19be45d6cf9bc84a65ef
Oyenola <nola1mz@gmail.com>
2026-07-29 01:20:18 +0100

    Add property input type

---
+++ types/property.ts
@@ -0,0 +1,12 @@
+export interface PropertyInput {
+  title: string;
+  description: string;
+  propertyType: string;
+  size: string;
+  location: string;
+  ownerName: string;
+  ownerEmail: string;
+  ownerPhone: string;
+  ownerWallet: string;
+  metadataHash: string;
+}
